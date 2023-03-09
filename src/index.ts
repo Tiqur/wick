4cbe06d50e8b6e66e8236332cd27b13a8dea84de
Tiqur <trevorbrage02@gmail.com>
2023-03-09 14:33:51 -0800

    Add FPS counter

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,31 @@ import Wick from './systems/wick';
 import { addStaticCandles, addDynamicCandle, OHLC } from './components/candle';
 import debugCandles from './assets/debugCandles.json';
 import { GUI } from 'dat.gui'
+import Stats from 'stats.js';
 
 // Reference to container element
 const container = document.getElementById("scene-container");
+
+// Init Wick class
 const wick = new Wick(container);
 
+const stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
+
+function animate() {
+
+	stats.begin();
+
+	// monitored code goes here
+
+	stats.end();
+
+	requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
 const camera = { x: 0, y: 0, z: 0 };
 const gui = new GUI()
 const cameraFolder = gui.addFolder('Camera')
