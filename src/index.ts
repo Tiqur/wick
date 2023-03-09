90edb69aa59937014db2adac351925ba63e41b5a
Tiqur <trevorbrage02@gmail.com>
2023-03-09 11:27:02 -0800

    Render basic scene

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import * as THREE from "three";
+
+
+// Get canvas element
+const canvas = document.getElementById("canvas");
+
+// Init renderer, camera, and scene
+const renderer = new THREE.WebGLRenderer({ canvas });
+const camera = new THREE.PerspectiveCamera(60, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+
+// Create Geometry and Material for scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+// Add object to scene
+scene.add(cube);
+
+// Position camera
+camera.position.z = 5;
+cube.position.x = -2;
+
+// Render
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+renderer.render(scene, camera);
